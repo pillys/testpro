23b22b78a2d861923d32904765746432de4fe891
Pillys <pillys@163.com>
2015-11-20 01:58:55 +0800

    submit

--- static/js/app.js
+++ static/js/app.js
@@ -8,7 +8,22 @@ var mainView = myApp.addView('.view-main', {
 });
 
 myApp.onPageInit('index', function(page) {
+  $$('.banner form').on('submit', function(e) {
+    var action = $$(this).attr('action'),
+      input = $$(this).find('input');
+    mainView.router.loadPage(action + '?' + input.attr('name') + '=' + encodeURIComponent(input.val()));
+    e.preventDefault();
+  });
+});
 
+myApp.onPageInit('pingjia-info', function(page) {
+  $$('.ui-pingjia-info-toolbar form').on('submitted', function(e) {
+    var data = e.detail.data; // Ajax repsonse from action file
+    data = JSON.parse(data);
+    if (data.flag === 1) {
+      myApp.alert('发布成功！', '提示信息');
+    }
+  });
 });
 
 myApp.init();
