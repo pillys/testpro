77cc2319f9e6c7eb923a182eb21c3c4b1d2a0c69
Pillys <pillys@163.com>
2015-11-20 01:15:25 +0800

    update

--- static/js/app.js
+++ static/js/app.js
@@ -7,22 +7,12 @@ var mainView = myApp.addView('.view-main', {
   dynamicNavbar: true
 });
 
-myApp.onPageInit('*', function(page) {
-  /*var url = page.url;
-  $$('.toolbar .toolbar-inner a.link').each(function(index, v) {
-    if ($$(this).attr('href').indexOf(url) !== -1) {
-      $$(this).addClass('active');
-    } else {
-      $$(this).removeClass('active');
-    }
-  });*/
-});
-
 myApp.onPageInit('index', function(page) {
-  /*var mySwiper1 = myApp.swiper('.swiper-1', {
-    pagination: '.swiper-1 .swiper-pagination',
-    spaceBetween: 50
-  });*/
+
 });
 
 myApp.init();
+
+if (location.pathname === '/') {
+  mainView.router.loadPage('/');
+}
